cfc11177327ab0d6b43147a4eaa74a397231831c
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2020-11-09 14:37:09 -0500

    add autocomplete

--- src/components/MockNav.js
+++ src/components/MockNav.js
@@ -3,12 +3,15 @@ import { Button } from 'react-bootstrap';
 import Nav from 'react-bootstrap/Nav'
 import '../App.css';
 import roofinglogo from '../roofinglogo.png'
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import { TextField } from '@material-ui/core';
+
 
 const MockNav = () => {
     return(
         
         <>
-        <div class="top-container-padding">
+        <div className="top-container-padding">
         <div className="top-nav">
             <div className="logo">
             <img src={roofinglogo} height="30px" />
@@ -33,16 +36,26 @@ const MockNav = () => {
         </div>
 
         <div className="title">
-            <h1 class="title-text">Free Roof Repair Inspection & Local Roofing Quote</h1>
+            <h1 className="title-text">Free Roof Repair Inspection & Local Roofing Quote</h1>
         </div>
 
         <div className="subtitle">
-            <h2 class="subtitle-text">Get your roof inspected remotely and connect with our trusted roofers</h2>
+            <h2 className="subtitle-text">Get your roof inspected remotely and connect with our trusted roofers</h2>
         </div>
 
-        <div class="address-form-div">
-            <input type="text"/>
-            <button >Get Inspection</button>
+        <div className="address-form-div">
+            {/* <input onFocus={handleFocus} type="text" autoComplete={addresses} /> */}
+
+            <Autocomplete
+                id="custom-input-demo"
+                options={addresses}
+                renderInput={(params) => (
+                    <div ref={params.InputProps.ref}>
+                    <input style={{ width: 300 }} type="text" {...params.inputProps} />
+                    </div>
+                )}
+            />
+            <button onClick={() => alert("hello")} >Get Inspection</button>
         </div>
         </div>
         
@@ -51,3 +64,13 @@ const MockNav = () => {
 }
 
 export default MockNav
+
+
+const addresses = [
+    '150 West Griffin Street Lawndale, CA 90260',
+'712 Anderson St. Holly Springs, NC 27540',
+'7673 High Ridge St. Deer Park, NY 11729',
+'63 Tower Street Riverview, FL 33569',
+'7755 Wall Street Baldwinsville, NY 13027',
+'434 West Monroe Road Camp Hill, PA 17011'
+]
