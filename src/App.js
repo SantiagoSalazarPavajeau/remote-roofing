666b560cfb47cf902ca1d536d88feef76eee33e4
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2020-11-09 14:54:12 -0500

    add elements to overlapping section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import RoofHeader from './components/RoofHeader';
-
+import AssignmentIcon from '@material-ui/icons/Assignment';
+import BuildIcon from '@material-ui/icons/Build';
+import PageviewIcon from '@material-ui/icons/Pageview';
 
 
 function App() {
@@ -11,7 +13,21 @@ function App() {
           
           <header className="white-space">
             <div className="overlap">
+              <div className="options">
+              <div className="options-text">
+                <PageviewIcon style={{ fontSize: 50 }} />
+                Free Inspection              
+              </div>
+              <div className="options-text">
+                <AssignmentIcon style={{ fontSize: 50 }}/>
+                File a Claim
+                </div>
+                <div className="options-text">
+                <BuildIcon style={{ fontSize: 50 }}/>
 
+                  Replace or Repair Roof
+                </div>
+                </div>
             </div>
           </header>          
       </header>
